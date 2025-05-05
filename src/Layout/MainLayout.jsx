b8bde1eb1b3a9cc16cd88bf5f3f72023a835f242
Jahangir-alam25 <jahangiralam10022003@gmail.com>
2025-05-05 12:29:06 +0800

    added footer section

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import { Outlet } from 'react-router';
+import Footer from '../Components/Footer';
 
 const MainLayout = () => {
     return (
@@ -11,6 +12,9 @@ const MainLayout = () => {
            <main className='w-11/12 mx-auto'>
            <Outlet></Outlet>
            </main>
+           <footer>
+            <Footer></Footer>
+           </footer>
         </div>
     );
 };
